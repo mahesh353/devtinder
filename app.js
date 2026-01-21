dca284abf990412eee1210540c089ef3876693f8
mahesh353 <pritampatil353@gmail.com>
2026-01-21 10:54:01 +0530

    Added mongoose package and database connectivity code

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-console.log("Starting a new project...");
-
-//import express from 'express'
-
 const express = require('express')
-
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+const connectDb = require('./config/database')
+
+connectDb().then(() => {
+  console.log('Connected to the database, starting the server...');
+  app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+});
+}).catch((err) => { console.log("Database connection error:", err); });
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
---
+++ config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const dbURI =
+  "mongodb+srv://maheshradheshyampatil353_db_user:TTH2oPSz3QtoXmM5@maheshnamastenode.pvysivz.mongodb.net/";
+
+const connectDb = async () => {
+  await mongoose
+    .connect(dbURI)
+    .then(() => console.log("Database connected successfully"))
+    .catch((err) => console.log("Database connection error:", err));
+};
+
+module.exports = connectDb;
