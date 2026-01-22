d2050af1de4d38c7db4a531c8b7f615cfcc43be8
mahesh353 <pritampatil353@gmail.com>
2026-01-22 11:58:58 +0530

    added delete & update user method

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ app.use(express.json());
 
 app.get("/user", async (req, res) => {
   const emailId = req.body.emailId;
-  console.log("Received emailId:", emailId);
   try {
     const user = await User.find({ emailId: emailId });
     if (user.length > 0) {
@@ -39,6 +38,37 @@ app.post("/signup", async (req, res) => {
     res.status(201).send("User created successfully");
   } catch (error) {
     res.status(500).send("Error creating user: " + error.message);
+  }
+});
+
+app.delete("/user/:id", async (req, res) => {
+  const userId = req.params.id;
+  try {
+    const deletedUser = await User.findByIdAndDelete(userId);
+    if (deletedUser) {
+      res.status(200).send("User deleted successfully");
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (error) {
+    res.status(500).send("Error deleting user: " + error.message);
+  }
+});
+
+app.patch("/user/:id", async (req, res) => {
+  const userId = req.params.id;
+  const updates = req.body;
+  try {
+    const updatedUser = await User.findByIdAndUpdate(userId, updates, {
+      new: true, runValidators: true
+    });
+    if (updatedUser) {
+      res.status(200).json(updatedUser);
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (error) {
+    res.status(500).send("Error updating user: " + error.message);
   }
 });
 
--- config/models/user.js
+++ config/models/user.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  firstName: { type: String },
+  firstName: { type: String, required: true },
   lastName: { type: String },
-  emailId: { type: String },
+  emailId: { type: String, lowercase: true, unique: true, required: true },
   password: { type: String },
-  age: { type: Number },
-  gender: { type: String },
-});
+  age: { type: Number, min: 18},
+  gender: { type: String, validate: function(v) { if (["male", "female", "other"].includes(v)) return true; return false; } },
+  bio: { type: String, default: "This is default Bio" },
+  profilePicture: { type: String },
+  skills: { type: [String] }
+}, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
