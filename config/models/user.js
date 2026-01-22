8fe7f9d11a0152b38579b4cbd8a5b515b37d17fd
mahesh353 <pritampatil353@gmail.com>
2026-01-22 12:20:09 +0530

    Added a validation for email using validator library

--- config/models/user.js
+++ config/models/user.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String },
-  emailId: { type: String, lowercase: true, unique: true, required: true },
+  emailId: { type: String, lowercase: true, unique: true, required: true, validate: function(v) { return validator.isEmail(v); } },
   password: { type: String },
   age: { type: Number, min: 18},
   gender: { type: String, validate: function(v) { if (["male", "female", "other"].includes(v)) return true; return false; } },
