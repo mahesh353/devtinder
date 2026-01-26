8221794b11fa232289feb8f46f349f156e57f88e
mahesh353 <pritampatil353@gmail.com>
2026-01-26 16:25:45 +0530

    Added one more endpoint

--- routes/user.js
+++ routes/user.js
@@ -19,4 +19,20 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   }
 });
 
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connections = await ConnectionRequest.find({
+      $or: [
+        { fromUserId: loggedInUser._id, status: "accepted" },
+        { toUserId: loggedInUser._id, status: "accepted" },
+      ],
+    }).populate("fromUserId", ["firstName", "lastName"]);
+
+    res.status(200).json({ message: connections });
+  } catch (error) {
+    res.status(500).json({ message: "Server error " + error.message });
+  }
+});
+
 module.exports = userRouter;
